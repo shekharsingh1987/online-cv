813b8fadaed7c3683c005bb27240d790d9234aac
shekharsingh1987 <shekharsingh_1987@yahoo.com>
2018-06-25 12:03:03 +0530

    Added the print profile script

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,24 +1,25 @@
-jQuery(document).ready(function($) {
+jQuery(document).ready(function ($) {
 
     $('.level-bar-inner').css('width', '0');
-    
-    $(window).on('load', function() {
 
-        $('.level-bar-inner').each(function() {
-        
+    $(window).on('load', function () {
+
+        $('.level-bar-inner').each(function () {
+
             var itemWidth = $(this).data('level');
-            
+
             $(this).animate({
                 width: itemWidth
             }, 800);
-            
+
         });
 
-        function PrintWholeDocument() {
+        $('#PrintWholeDocument').click(function () {
             window.print();
-        }
+        });
+
     });
-   
-    
+
+
 
 });
